908fedac32b2b0a593bfea8ff356fedefbd43ee9
Wellington Tiago da Silva <wellington.developer@gmail.com>
2025-06-03 00:33:34 +0000

    Sincroniza

--- projeto/projeto_python/exercicio2.py
+++
@@ -1,6 +0,0 @@
-nome = 'Wellington Silva'
-altura = 1.87
-peso = 93
-imc = peso // (altura * altura)
-
-print('Meu nome é', nome, 'tenho', altura, 'e peso', peso, ',meu IMC é', imc )
--- projeto/projeto_python/str.py
+++
@@ -1,5 +0,0 @@
-# SEP para alterar o separador
-# END para alterar o final da linha
-# Só funciona no funcao print
-
-print(1,  "Hello World", sep="=", end="\n")
